a1262099a86dc93b458667ce833d0979ad5dd3fc
Ansh Mahajan <11aanshmahajan@gmail.com>
2025-10-24 22:35:35 +0530

    Minor impot fixes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,6 @@
 // backend/middleware/auth.js
 import jwt from "jsonwebtoken";
-import User from "../models/user.js";
+import User from "../models/User.js";
 
 export async function protect(req, res, next) {
   const authHeader = req.headers.authorization;
